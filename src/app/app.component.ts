433f019d375332fa6227b2aa2d4098f7bb5b6754
Carlos Palomino <carlos.e93@hotmail>
2017-07-12 16:52:03 -0500

    enlistar cualquier pagina y ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,51 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Pipe, PipeTransform } from '@angular/core';
+import {Injectable} from '@angular/core'
+import 'rxjs/Rx';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+@Injectable()
+// export class ServerService{
+
+
+// };
+
 export class AppComponent {
-	username="";
-constructor(http: Http){
-	
-	http.get("https://wakatime.com/api/v1/leaders?page=2")
-	.subscribe(
-		(res: Response)=> 
-		{
-			const city = res.json().data;
-			console.log(city);
-		this.username=city.rank;
-		})
+servers: string[] = new Array();
+// constructor(http: Http){
 
-}
+// 	http.get("https://wakatime.com/api/v1/leaders?page=2")
+// 	.subscribe(
+// 		(res: Response)=> 
+// 		{
+	// 			const city = res.json().data;
+	// 			console.log(city);
+	// 		this.username=city.rank;
+	// 		})
 
+	// };
+	constructor(private http: Http){}
+	getServers(id: number){
+		return this.http.get("https://wakatime.com/api/v1/leaders?page="+id)
+		.map(
+			(response:Response)=>{
+				const data=response.json().data;
+				return data;
+			})
+	}
+
+	onGet(){
+		this.getServers(13)
+		.subscribe(
+			(servers: any[])=>{this.servers =servers;
+			console.log(servers)},
+			(error)=>console.log(error)
+			)
+
+
+	}
 }
