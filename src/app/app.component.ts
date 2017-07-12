3b481f45ae869ffe5b3bbfdd39a7463d769590e5
Carlos <30105417+carlospalo@users.noreply.github.com>
2017-07-12 13:44:53 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import { Pipe, PipeTransform } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	username="";
+constructor(http: Http){
+	
+	http.get("https://wakatime.com/api/v1/leaders?page=2")
+	.subscribe(
+		(res: Response)=> 
+		{
+			const city = res.json().data;
+			console.log(city);
+		this.username=city.rank;
+		})
+
+}
+
 }
